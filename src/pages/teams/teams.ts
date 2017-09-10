e5480650724f5dcf7908ac88fa6c9d1a339a63d3
suprince.shakya <suprince.shakya@rts.com.np>
2017-09-10 15:06:35 +0545

    search bar added

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -13,6 +13,8 @@ export class TeamsPage {
   private allTeams:any;
   private allTeamDivisions:any;
   teams=[];
+  queryText:string;
+
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private eliteApi:EliteApi,
@@ -45,5 +47,17 @@ export class TeamsPage {
 
   itemTapped($event,team){
     this.navCtrl.push(TeamHomePage,team);
+  }
+
+  updateTeams(){
+    let queryTextLower=this.queryText.toLowerCase();
+    let filteredTeams=[];
+    _.forEach(this.allTeamDivisions,td=>{
+      let teams=_.filter(td.divisionTeams,t=>(<any>t).name.toLowerCase().includes(queryTextLower));
+      if(teams.length){
+        filteredTeams.push({divisionName:td.divisionName,divisionTeams:teams})
+      }
+    });
+    this.teams=filteredTeams;
   }
 }
